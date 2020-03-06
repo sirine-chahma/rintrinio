cbdf5b71f15f5ff85d6605e17b4052bd045866bb
hatchi28 <aakanksha023@gmail.com>
2020-03-06 12:56:18 -0800

    function added

--- R/rintrinio.R
+++ R/rintrinio.R
@@ -1,8 +1,10 @@
+library(IntrinioSDK)
 library(tidyverse)
+options(warn=-1)
 
 # Function that gathers a given financial statement for a company for specificed years and quarters
 
-#' Given the tickers, statement, year and period returns all the financial information from the Intrinio API stock data 
+#' Given the tickers, statement, year and period returns complete available financial information from the Intrinio API stock data 
 #'
 #' @param api_key character (sandbox or production) from Intrinio
 #' @param ticker ticker symbol
@@ -10,15 +12,117 @@ library(tidyverse)
 #' @param year vector of the years (as characters) you want the information for
 #' @param period vector of the periods (characters) you want the information from
 #'
-#' @return a dataframe that contains information about the given statement for a given ticker for the specified years
+#' @return a dataframe that contains information about the company for a given statement for a given ticker for the specified years
 
 #' @export
 #' @examples
-#' gather_financial_statement_time_series(api_key, 'AAPL', 'income_statement', ['2018,'2019'], ['Q1'])
+#' gather_financial_statement_time_series(api_key, 'CVX', 'income_statement', c('2017,'2018'), c('Q1','Q3'))
 
 gather_financial_statement_time_series <- function(api_key, ticker, statement, year, period){
-  tibble()
+  
+  ## Limited free access on intrino provides only Sandbox access
+  
+  ## Checks
+  
+  if (typeof(api_key) != "character"){
+    stop("The api_key must be a string")
+  }
+  
+  if (typeof(ticker) != "character") {
+    stop("The ticker must be a string. For ex. 'CSCO'")
+  }
+  
+  if (typeof(statement) != "character") {
+    stop("The statement must be a string. For ex. 'cash_flow_statement'")
+  }
+  
+  if (typeof(year) != "character") {
+    stop("The year must be a vector of string(s). For ex. c('2016', '2018')")
+  }
+  
+  for(y in year){
+    if(nchar(y) != 4){
+      stop("Sorry, year must be a string of 4 digits")
+    }
+  }
+  
+  if (typeof(period) != "character") {
+    stop("The year must be a vector of string(s). For ex. c('Q1', 'Q3')")
+  }
+  
+  available_statements <- c('income_statement', 'cash_flow_statement', 'balance_sheet_statement')
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (statement %notin% c('income_statement', 'cash_flow_statement', 'balance_sheet_statement')) {
+    stop("Valid entries for statement are 'income_statement', 'cash_flow_statement' or 'balance_sheet_statement'")
+  }
+  
+  ## List of available tickers with sandbox key: 
+  ## https://product.intrinio.com/developer-sandbox/coverage/us-fundamentals-financials-metrics-ratios-stock-prices
+  
+  available_tickers = c('AAPL', 'AXP', 'BA', 'CAT', 'CSCO', 'CVX', 'DIS', 'DWDP', 'GE', 'GS', 'HD', 'IBM', 'INTC', 'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PFE', 'PG', 'TRV', 'UNH', 'UTX', 'V', 'VZ', 'WMT', 'XOM')
+  
+  if (ticker %notin% available_tickers) {
+    stop("Valid entries for ticker provided in the Readme.md")
+  }
+  
+  client <- IntrinioSDK::ApiClient$new()
+  
+  ## Configure API key authorization: ApiKeyAuth
+  client$configuration$apiKey <- api_key
+  
+  ## Setup API with client
+  FundamentalsApi <- IntrinioSDK::FundamentalsApi$new(client)
+  
+  ## final dataframe to be joined to this
+  my_results = c()
+  
+  for(i in year) {
+    for(j in period) {
+      
+      ## set key to obtain relevant information
+      key <- paste(ticker, statement, i, j, sep='-')
+      
+      fundamentals <- FundamentalsApi$get_fundamental_standardized_financials(key)
+      
+      temp_result <- c(c('ticker',ticker), c('statement', statement), c('year', i), c('period', j))  
+      
+      for (k in 1:(length(fundamentals$content$standardized_financials))) {
+        value <- fundamentals$content$standardized_financials[[k]]$value
+        data_tag <- fundamentals$content$standardized_financials[[k]]$data_tag$tag
+        balance <- fundamentals$content$standardized_financials[[k]]$data_tag$balance          
+        
+        if (balance == "credit" || is.na(balance)  ){
+          value = -value
+        }
+        
+        temp_result <- c(temp_result, c(data_tag, value))
+        #print(temp_result)
+      }
+      
+      type <- c()
+      fin_value <- c()
+      
+      for (n in seq(length(temp_result)/2)){
+        type <- c(type, temp_result[2*n-1])
+        fin_value <- c(fin_value, temp_result[2*n]) 
+      }
+      
+      final_data <- data.frame(type, fin_value)
+      
+      if(length(my_results)==0){
+        my_results <- final_data
+      }
+      else{
+        my_results <- full_join(my_results, final_data, by='type', suffix = c("", "."))
+      } 
+    }   
+    
+  }
+  return(my_results)
 }
+
 
 
 # Function that gathers a given statement at a specific time for different companies
