9d751ffa594c35ab291548a4d76f5a2f45e8b932
sirine_chahma <sirine.chahma@supelec.fr>
2020-03-13 09:46:13 -0700

    fix indentation

--- R/rintrinio.R
+++ R/rintrinio.R
@@ -1,11 +1,11 @@
 # library(tidyverse) # according to slack, it's suggested not to load the whole tidyverse
 library(IntrinioSDK)
 library(dplyr)
-library(tidyr) 
+library(tidyr)
 
 # Function that gathers a given financial statement for a company for specificed years and quarters
 
-#' Given the tickers, statement, year and period returns all the financial information from the Intrinio API stock data 
+#' Given the tickers, statement, year and period returns all the financial information from the Intrinio API stock data
 #'
 #' @param api_key character (sandbox or production) from Intrinio
 #' @param ticker ticker symbol
@@ -40,66 +40,66 @@ gather_financial_statement_time_series <- function(api_key, ticker, statement, y
 #' gather_financial_statement_company_compare(api_key, ['AAPL', 'CSCO'], 'income_statement', '2019', 'Q1')
 
 gather_financial_statement_company_compare <- function(api_key, ticker, statement, year, period){
-  
+
   statements <-  c('income_statement', 'balance_sheet_statement', 'cash_flow_statement')
-  
+
   #Check if the statement is valid
   if (!(statement %in% statements)){
     stop('the statement must exist')
   }
-  
+
   #check if the api_key is a string
   if (typeof(api_key) != "character"){
     stop("the api_key must be a string")
   }
-  
+
   #check if the ticker is a string
   if (typeof(ticker) != "character") {
     stop("the ticker must be a string")
   }
-  
+
   #check if the statement is a string
   if (typeof(statement) != "character") {
     stop("the statement must be a string")
   }
-  
+
   #check if the year is a string
   if (typeof(year) != "character") {
     stop("the year must be a string")
   }
-  
+
   #check if the period is a string
   if (typeof(period) != "character") {
     stop("the period must be a string")
   }
-  
-  
+
+
   client <- IntrinioSDK::ApiClient$new()
-  
+
   # Configure API key authorization: ApiKeyAuth
   client$configuration$apiKey <- api_key
-  
+
   # Setup API with client
   FundamentalsApi <- IntrinioSDK::FundamentalsApi$new(client)
-  
+
   #final dataframe
   result = c()
-  
+
   for (comp in seq(length(ticker))){
-    
+
     #set the id
     id <- paste(ticker[comp], statement, year, period, sep='-')
-    
+
     response <- FundamentalsApi$get_fundamental_standardized_financials(id)
-    
+
     #create a vector for each information
     my_list <- c(c('ticker',ticker[comp]), c('statement', statement), c('year', year), c('period', period))
-    
+
     for (i in 1:(length(response$content$standardized_financials))){
       value <- response$content$standardized_financials[[i]]$value
       data_tag <- response$content$standardized_financials[[i]]$data_tag$tag
       balance <- response$content$standardized_financials[[i]]$data_tag$balance
-      
+
       if (is.na(balance) || balance=="credit"){
         value = -value
       }
@@ -110,7 +110,7 @@ gather_financial_statement_company_compare <- function(api_key, ticker, statemen
     name <- c()
     #value contains the values of those tags
     value <- c()
-    
+
     for (j in seq(length(my_list)/2)){
       name <- c(name, my_list[2*j-1])
       value <- c(value, my_list[2*j])
@@ -130,7 +130,7 @@ gather_financial_statement_company_compare <- function(api_key, ticker, statemen
 
 # Function that gathers historical stock price information
 
-#' Given the ticker, start date, and end date, return from the Intrinio API stock data 
+#' Given the ticker, start date, and end date, return from the Intrinio API stock data
 #' for that time frame in either a dictionary or a pandas dataframe format.
 #'
 #' @param api_key character (sandbox or production) from Intrinio
@@ -144,7 +144,7 @@ gather_financial_statement_company_compare <- function(api_key, ticker, statemen
 #' @examples
 #' gather_stock_time_series(api_key, 'AAPL', "2017-12-31", "2019-03-01")
 gather_stock_time_series <- function(api_key, ticker, start_date='', end_date='') {
-  
+
   # set up options
   if (start_date != '' & end_date != '') {
     opts <- list(start_date = as.Date(start_date), end_date = as.Date(end_date), page_size = 10000)
@@ -158,19 +158,19 @@ gather_stock_time_series <- function(api_key, ticker, start_date='', end_date=''
   else {
     opts <- list(page_size = 10000)
   }
-  
+
   # throw an error if the API Key is Invalid
   api_error <- try({
     client <- IntrinioSDK::ApiClient$new()
     client$configuration$apiKey <- api_key
     SecurityApi <- IntrinioSDK::SecurityApi$new(client)
     result <- SecurityApi$get_security_stock_prices(ticker, opts)$content$stock_prices_data_frame
-    
+
   }, silent=T)
   if(is.null(result)) {
     return("Incorrect API Key - please input a valid API key as a string")
   }
-  
+
   return(result)
 
 
@@ -180,10 +180,10 @@ gather_stock_time_series <- function(api_key, ticker, start_date='', end_date=''
 #'
 #' @param api_key character (sandbox or production) from Intrinio
 #' @param ticker character ticker symbols or a vector of ticker symbols
-#' @param buy_date character the buy-in date in the format of "%Y-%m-%d", e.g. "2019-12-31" 
-#' If the input date is not a trading day, it will be automatically changed to the next nearest trading day. 
+#' @param buy_date character the buy-in date in the format of "%Y-%m-%d", e.g. "2019-12-31"
+#' If the input date is not a trading day, it will be automatically changed to the next nearest trading day.
 #' @param sell_date character the sell-out date in the format of "%Y-%m-%d", e.g. "2019-12-31"
-#' If the input date is not a trading day, it will be automatically changed to the last nearest trading day. 
+#' If the input date is not a trading day, it will be automatically changed to the last nearest trading day.
 #'
 #' @return a dataframe that contains the companies, historical prices and corresponding
 #' profit/loss
@@ -193,17 +193,17 @@ gather_stock_time_series <- function(api_key, ticker, start_date='', end_date=''
 #' gather_stock_returns(api_key, c('AAPL', 'CSCO'), "2017-12-31", "2019-03-01")
 
 gather_stock_returns <- function(api_key, ticker, buy_date, sell_date) {
-  
+
   # test whether the input dates are in the right format
   t <- try({buy_date <- as.Date(buy_date)
             sell_date <- as.Date(sell_date)}, silent=T)
   if("try-error" %in% class(t)) {
       return("Invalid Date format - please input the date as a string with format %Y-%m-%d")
-  }    
+  }
   if (buy_date >= sell_date){
       return("Invalid Input: `sell_date` is earlier than `buy_date`.")
   }
-  
+
   client <- IntrinioSDK::ApiClient$new()
   # Configure API key authorization: ApiKeyAuth
   client$configuration$apiKey <- api_key
@@ -215,20 +215,20 @@ gather_stock_returns <- function(api_key, ticker, buy_date, sell_date) {
   buy_date <- as.Date(buy_date)
   buy_date_upper <- buy_date + 10
   sell_date <- as.Date(sell_date)
-  sell_date_lower <- sell_date - 10 
-  
+  sell_date_lower <- sell_date - 10
+
   # test if the API Key works
   t <- try({opts <- list(start_date=buy_date, end_date=sell_date)
             x <- SecurityApi$get_security_stock_prices('AAPL', opts)$content$stock_prices_data_frame$adj_close}, silent=T)
   if(is.null(x)) {
       return("Incorrect API Key - please input a valid API key as a string")
-  } 
+  }
 
   # create vectors to record the results
   rcd_buy_price <- c()
   rcd_sell_price <- c()
   rcd_rtn <- c()
-      
+
   for (x in ticker){
 
     opts <- list(start_date=buy_date, end_date=buy_date_upper)
@@ -247,13 +247,11 @@ gather_stock_returns <- function(api_key, ticker, buy_date, sell_date) {
 
   }
 
-  result <- tibble('Stock' = ticker, 
-          'Buy date' = buy_date, 
-          'Buy price' = rcd_buy_price, 
-          'Sell date' = sell_date, 
-          'Sell price' = rcd_sell_price, 
+  result <- tibble('Stock' = ticker,
+          'Buy date' = buy_date,
+          'Buy price' = rcd_buy_price,
+          'Sell date' = sell_date,
+          'Sell price' = rcd_sell_price,
           'Return (%)' = rcd_rtn
         )
-
-  return(result)
 }
